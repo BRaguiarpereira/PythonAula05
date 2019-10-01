2bd459ab3b9148302f35731a68dd0033098cf433
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-10-01 19:33:44 -0300

    explicaçao

---
+++ explicacao.py
@@ -0,0 +1,20 @@
+"""
+Comando for - repetiçao
+
+-contador obrigatorio;
+-numero de reptiçao conhecido;
+-o contador assume valores dentro de um intervalo definido no comando;
+
+Parametros: valor inicial do contador , valor final do contador e
+variaçao do contador a cada ciclo de repetiçao.
+
+"""
+
+"""
+for contador in range(valorinicial,valorfinal):
+    instruçoes
+"""
+# o terceiro valor é a variaçao de quanto em quanto ele vai executar o for
+
+for cont in range (0,100,3):
+    print(cont)
