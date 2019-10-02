a3fb4d1f87438d3fa8b863b1d0c8ffd77a371399
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-10-01 21:16:31 -0300

    arrayConceito

---
+++ Array.py
@@ -0,0 +1,28 @@
+'''
+Lista
+
+variavel = [valor1,valor2]
+
+ex:
+
+x=[2,3,4,5,7,10]
+
+podemos usar uma lista como intervalo para o comando for
+
+- contador assume , um decada vez os valores da lista.
+
+- o numero de repetiçoes é o numero de lementos da lista.
+
+'''
+
+'''
+i = 1
+
+for x in [2,5,7,12,14,21]:
+    print('Elemento{} : {}'.format(i,x))
+    i=i+1
+'''
+
+y=['noite','unip','energetico']
+for x in y:
+    print(x)
